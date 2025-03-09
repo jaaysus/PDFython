bd51be7cc0c24edba7eb326753312e4f99633c1f
jaaysus <issaelkaddaoui@gmail.com>
2025-03-09 13:52:01 +0000

    cleaned

--- gui/file_dialogs.py
+++
@@ -1,7 +0,0 @@
-from tkinter import filedialog
-
-def open_file_dialog(file_types):
-    return filedialog.askopenfilename(filetypes=file_types)
-
-def save_file_dialog(default_ext, file_types):
-    return filedialog.asksaveasfilename(defaultextension=default_ext, filetypes=file_types)
